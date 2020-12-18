135e7e14b57e9bc4eb1eb7349506b4a9bc976816
dominickreever <dominickmreever@gmail.com>
2020-12-18 09:16:13 -0500

    Connected database and added models

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// connect to database
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://user0:$database1@cluster0.9aijh.mongodb.net/locallib?retryWrites=true&w=majority'
+
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
